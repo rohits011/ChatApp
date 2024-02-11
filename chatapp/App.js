934dc5e9c11ec1c9cc8a39f90aa808131b6bc7fb
rohit singh <32682154+rohits011@users.noreply.github.com>
2024-02-11 06:06:37 +0000

    first commit

---
+++ chatapp/App.js
@@ -0,0 +1,16 @@
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>chat application
+      </Text>
+    </View>
+  );
+}
